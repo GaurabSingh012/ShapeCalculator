80137ac66740a6213a6d0e0866da2fc907efc026
Gaurab <gaurabkus781@gmail.com>
2025-04-19 20:31:03 +0530

    Take the input from the user L

--- main.java
+++ main.java
@@ -1,3 +1,12 @@
+import java.util.Scanner;
+
 public class main {
-    
+    public static void main(String[] args) {
+        Scanner scan  = new Scanner(System.in);
+        System.out.println("Enter the length of Rectangle: ");
+        double length = Double.parseDouble(scan.nextLine());
+
+        
+    }
+
 }
