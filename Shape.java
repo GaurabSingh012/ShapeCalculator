9b2def22cb34228ede75cbb7735fd14b19f54c1c
Gaurab <gaurabkus781@gmail.com>
2025-04-19 20:22:26 +0530

    Add constructor and protected fields to initialize shape dimensions and sides '

--- Shape.java
+++ Shape.java
@@ -1,4 +1,11 @@
 abstract class Shape{
     abstract double calculateArea();
     abstract double calculatePerimeter();
+
+    protected double dim_one, dim_two, dim_three;
+    int num_sides;
+    public Shape(){
+        dim_one = dim_two = dim_three = 0;
+        num_sides = 0;
+    }
 }
